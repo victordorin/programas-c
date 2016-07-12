a0669c766c19fb82a6c202472eefd91e9d486d71
victordorin <victordorin888@gmail.com>
2016-07-12 15:15:13 +0200

    problemas

---
+++ 10_problemas_de_salida_entrada/01_getchar_putchar.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+main(){
+char a, b, c;
+ a = getchar();
+ b = getchar();
+ c = getchar();
+
+ putchar(a);
+ putchar(b);
+ putchar(c);
+
+}
---
+++ 10_problemas_de_salida_entrada/02_printf_scanf.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+main()
+{
+    char a[2];
+   char  b, c;
+    
+    scanf(" %[^\n]", a);
+
+   printf("%s\n", a);
+   return 0;
+}
